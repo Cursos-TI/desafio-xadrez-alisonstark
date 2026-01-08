8c632f439936a8c3f9a87f481dde6edef575dc46
Alison Caique <58669866+alisonstark@users.noreply.github.com>
2026-01-08 18:47:38 +0000

    Definicoes iniciais do jogo, com adicao de menu usando switch. Solucao do desafio nivel novato.

--- xadrez.c
+++ xadrez.c
@@ -28,5 +28,125 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
+    // Defina as direcoes
+    char cima = 'c';
+    char baixo = 'b';
+    char direita = 'd';
+    char esquerda = 'e';
+    
+    // Declare as variaveis de direcao da peca escolhida pelo usuario
+    char direcao_torre;
+    char direcao_rainha;
+    char direcao_bispo_cb; // cima ou baixo
+    char direcao_bispo_ed; // esquerda ou direita
+
+    
+    // Defina as variaveis de numero de casas definido pelo usuario
+    unsigned short num_casas_torre;
+    unsigned short num_casas_rainha;
+    unsigned short num_casas_bispo;
+
+    // Logica pra o menu de selecao da direcao aqui
+    printf("Escolha a direcao e a quantidade de casas para movimentar cada peca.\n");
+    printf("Use:\n");
+    printf("e - esquerda\nd - direita\nc - cima\nb - baixo\n");
+    printf("Exemplo de resposta, 5 casas para a esquerda: e 5\n\n");
+    printf("No caso do Bispo, escolha a combinacao cima ou baixo com esquerda ou direita pra se mover na diagonal.\n");
+    printf("Exemplo de resposta para o Bispo, 4 casas na diagonal para cima a direita: c e 4\n\n");
+
+    printf("### Inicio de movimentacao das pecas ###\n\n");
+    printf("Movimente a Torre: ");
+    scanf(" %c %hu", &direcao_torre, &num_casas_torre); // Adicione espaco antes de %c para consumir algum espaco remanescente
+    printf("\n");
+
+    
+    printf("Movimente o Bispo: ");
+    scanf(" %c %c %hu", &direcao_bispo_cb, &direcao_bispo_ed, &num_casas_bispo);
+    printf("\n");
+
+    printf("Movimente a Rainha: ");
+    scanf(" %c %hu", &direcao_rainha, &num_casas_rainha);
+    printf("\n");
+
+    // Possivel melhoria: o usuario pode escolher a peca que quer movimentar
+
+    
+    // Defina o movimento da Torre
+    while(num_casas_torre > 0){
+        executar_mov_vert_hori(direcao_torre);
+        num_casas_torre--;
+    }
+    printf("\n### Finalizado o movimento da Torre ###\n\n");
+
+    // Defina o movimento do Bispo
+    do {
+        executar_mov_diag(direcao_bispo_cb, direcao_bispo_ed);
+        num_casas_bispo--;
+    }
+    while(num_casas_bispo > 0);
+
+    printf("\n### Finalizado o movimento do Bispo ###\n\n");
+    // Defina o movimento da rainha
+    for(unsigned short i=num_casas_rainha; i > 0; i--){
+        executar_mov_vert_hori(direcao_rainha);
+    }
+
+    printf("\n### Finalizado o movimento da Rainha ###\n\n");
+
     return 0;
 }
+// ##### Area de definicao de funcoes auxiliares
+// Funcao auxiliar para se mover na horizontal ou vertical
+void executar_mov_vert_hori(char direcao){
+    switch (direcao)
+        {
+        case 'd':
+            printf("Direita\n");
+            break;
+        
+        case 'e':
+            printf("Esquerda\n");
+            break;
+        
+        case 'c':
+            printf("Cima\n");
+            break;
+        
+        case 'b':
+            printf("Baixo\n");
+            break;
+
+        default:
+            printf("Direcao invalida! Tente novamente.\nSaindo...\n\n");
+            break;
+        }
+}
+
+// Funcao auxiliar para se mover na diagonal
+void executar_mov_diag(char direcao_cb, char direcao_ed){
+    switch (direcao_cb)
+        {
+        case 'c':
+            if(direcao_ed == 'e'){
+                printf("Cima Esquerda\n");
+            }
+            else if(direcao_ed == 'd'){
+                printf("Cima Direita\n");
+            }
+            break;
+        
+        case 'b':
+            if(direcao_ed == 'e'){
+                printf("Baixo Esquerda\n");
+            }
+            else if(direcao_ed == 'd'){
+                printf("Baixo Direita\n");
+            }
+            break;
+
+        default:
+            printf("Direcao invalida! Tente novamente.\nSaindo...\n\n");
+            break;
+        }
+}
+
