1263a5107842cec4a88e6cc423ee46df70d1f3ce
Alison Caique <58669866+alisonstark@users.noreply.github.com>
2026-01-10 01:39:59 +0000

    Ajustes pontuais.

--- xadrez.c
+++ xadrez.c
@@ -4,6 +4,64 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
+
+// ##### Área de definição de funções auxiliares
+// Função auxiliar para se mover na horizontal ou vertical
+void executar_mov_vert_hori(char direcao){
+    switch (direcao)
+        {
+        case 'd':
+            printf("Direita\n");
+            break;
+        
+        case 'e':
+            printf("Esquerda\n");
+            break;
+        
+        case 'c':
+            printf("Cima\n");
+            break;
+        
+        case 'b':
+            printf("Baixo\n");
+            break;
+
+        default:
+            printf("Direção inválida! Tente novamente.\nSaindo...\n\n");
+            break;
+        }
+}
+
+// Função auxiliar para se mover na diagonal
+void executar_mov_diag(char direcao_cb, char direcao_ed){
+    switch (direcao_cb)
+        {
+        case 'c':
+            if(direcao_ed == 'e'){
+                printf("Cima Esquerda\n");
+            }
+            else if(direcao_ed == 'd'){
+                printf("Cima Direita\n");
+            }
+            break;
+        
+        case 'b':
+            if(direcao_ed == 'e'){
+                printf("Baixo Esquerda\n");
+            }
+            else if(direcao_ed == 'd'){
+                printf("Baixo Direita\n");
+            }
+            break;
+
+        default:
+            printf("Direção Inválida! Tente novamente.\nSaindo...\n\n");
+            break;
+        }
+}
+
+// Função principal (main)
+
 int main() {
 
     // Nível Aventureiro - Movimentação do Cavalo
@@ -17,13 +75,13 @@ int main() {
     // Sugestão: Implemente a movimentação do Cavalo utilizando loops com variáveis múltiplas e condições avançadas.
     // Inclua o uso de continue e break dentro dos loops.
 
-    // Defina as direcoes
+    // Defina as direçõees
     char cima = 'c';
     char baixo = 'b';
     char direita = 'd';
     char esquerda = 'e';
     
-    // Declare as variaveis de direcao da peca escolhida pelo usuario
+    // Declare as variáveis de direção da peça escolhida pelo usuário
     char direcao_torre;
     char direcao_rainha;
     char direcao_bispo_cb; // cima ou baixo
@@ -32,61 +90,59 @@ int main() {
     char direcao_cavalo_1; // 1 casa perpendicular
 
     
-    // Defina as variaveis de numero de casas definido pelo usuario
+    // Defina as variáveis de número de casas definido pelo usuário
     unsigned short num_casas_torre;
     unsigned short num_casas_rainha;
     unsigned short num_casas_bispo;
 
-    // Logica pra o menu de selecao da direcao aqui
-    // Defina um menu de ajuda caso o usuario precise saber a forma de movimento da peca
-      
-    printf("Ao digitar a direcao, considere:\n");
-    printf("e - esquerda\nd - direita\nc - cima\nb - baixo\n");
+    // Defina um menu de ajuda caso o usuário precise saber a forma de movimento da peça
     printf("#### Game Start ####\n");
-    printf("Digita o numero da peca cujo movimento voce gostaria de conferir, ou 0 para avancar.\n");
-    unsigned short escolha = 1; // Defina um valor para entrar no loop
-    while(escolha != 0){
+    printf("Digite o número da peça, cujo movimento você gostaria de conferir, ou 0 para avançar.\n");
+    unsigned short control_loop = __SHRT_MAX__; // Defina um valor para entrar no loop
+    while(control_loop != 0){
         printf("(1)\t\tTorre\n");
         printf("(2)\t\tBispo\n");
         printf("(3)\t\tRainha\n");
         printf("(4)\t\tCavalo\n");
-        printf("(0)\t\tAvancar\n");
+        printf("(0)\t\tAvançar\n");
         printf("Escolha: ");
-        scanf("%hu", &escolha);
-
-        switch (escolha)
-        {
-        case 1:
-            printf("Movimenta-se horizontal e verticalmente, varias casas. Indique a direcao e o numero de casas.\n");
-            break;
         
-        case 2:
-            printf("Movimenta-se diagonalmente. Para esse movimento, escolha a combinacao 'c' ou 'b' com 'e' ou 'd' pra se mover na diagonal.\n");
-            printf("Exemplo de resposta para o Bispo, 4 casas na diagonal para cima a direita: c e 4\n");
-            break;
+        scanf("%hu", &control_loop);
         
-        case 3:
-            // Para simplificar, limite o movimento da Rainha a vertical ou horizontal
-            // TODO Implementar o movimento real da Rainha
-            printf("Movimenta-se varias casas para a esquerda ou direita.\n");
-            // printf("Para o movimento diagonal, escolha a combinacao 'c' ou 'b' com 'e' ou 'd' pra se mover na diagonal.\n");
-            break;
-        
-        case 4:
-            printf("O Cavalo se movimenta em 'L'.\n");
-            printf("Escolha a direcao do movimento de 2 casas, 'c', 'b', 'e' ou 'd'. Para completar o movimento, escolha 'e' ou 'd'.\n");
-            break;
-        
-        default:
-            break;
+        printf("Ao digitar a direção, considere:\n");
+        printf("e - esquerda\nd - direita\nc - cima\nb - baixo\n");
+        switch (control_loop){
+            case 1:
+                printf("Movimenta-se horizontal e verticalmente, várias casas. Indique a direção e o número de casas.\n");
+                printf("Exemplo de resposta para a Torre, 3 casas na vertical para cima: c 3\n");
+                break;
+            
+            case 2:
+                printf("Movimenta-se diagonalmente. Para esse movimento, escolha a combinação 'c' ou 'b' com 'e' ou 'd'.\n");
+                printf("Exemplo de resposta para o Bispo, 4 casas na diagonal para cima a direita: c e 4\n");
+                break;
+            
+            case 3:
+                // Para simplificar, limite o movimento da Rainha a vertical ou horizontal
+                // TODO Implementar o movimento real da Rainha (requer estrutura de dados composta)
+                printf("Movimenta-se varias casas para a esquerda ou direita.\n");
+                printf("Exemplo de resposta para a Rainha, 6 casas para esquerda: e 6\n");
+                break;
+            
+            case 4:
+                printf("O Cavalo se movimenta em 'L'.\n");
+                printf("Escolha a direção do movimento de 2 casas: 'c', 'b', 'e' ou 'd'. Para a última casa, escolha 'e' ou 'd'.\n");
+                break;
+            
+            default:
+                break;
         }
-        
     }
 
-    printf("### Inicio de movimentacao das pecas ###\n");
-    printf("Escolha a peca que deseja movimentar, ou digite 0 para sair do jogo.\n\n");
-    unsigned short escolha_peca;
-    while(1){
+    printf("### Início de movimentação das peças ###\n");
+    printf("Escolha a peça que deseja movimentar, ou digite 0 para sair do jogo.\n\n");
+    unsigned short escolha_peca = __SHRT_MAX__;
+    while(escolha_peca != 0){
         printf("(1)\t\tTorre\n");
         printf("(2)\t\tBispo\n");
         printf("(3)\t\tRainha\n");
@@ -131,7 +187,7 @@ int main() {
             scanf(" %c %hu", &direcao_rainha, &num_casas_rainha);
             printf("\n");
 
-            // Defina o movimento da rainha
+            // Defina o movimento da Rainha
             for(unsigned short i=num_casas_rainha; i > 0; i--){
                 executar_mov_vert_hori(direcao_rainha);
             }
@@ -140,84 +196,29 @@ int main() {
             break;
         
         case 4:
-            
             printf("Movimente o Cavalo: ");
             scanf(" %c %c", &direcao_cavalo_3, &direcao_cavalo_1);
             printf("\n");
-            unsigned short stop;
+
+            // Defina o movimento do Cavalo
+            unsigned short stop;    // Condição de parada para o loop interno
             for (unsigned short i = 2; i > 0; i--){
                 // Poderia ser um simples if(i == 0) then executar_mov_vert_hori(direcao_cavalo_1)
-                // Crie um loop while para movimentar o Cavalo 2 casas na vert ou hori e depois 1 casa perpendicular
                 stop = 1;
                 while(stop < 2){
-                    executar_mov_vert_hori(direcao_cavalo_3);
+                    executar_mov_vert_hori(direcao_cavalo_3);   // "Perna maior" do 'L'
                     stop++;
                 }
             }
-            executar_mov_vert_hori(direcao_cavalo_1);
+            executar_mov_vert_hori(direcao_cavalo_1);           // "Perna menor" do 'L'
             printf("\n### Finalizado o movimento do Cavalo ###\n\n");
             break;
         
         default:
+            printf("\n### Fim do jogo!! ###\n\n");
             break;
         }
         
     }
-
     return 0;
 }
-// ##### Area de definicao de funcoes auxiliares
-// Funcao auxiliar para se mover na horizontal ou vertical
-void executar_mov_vert_hori(char direcao){
-    switch (direcao)
-        {
-        case 'd':
-            printf("Direita\n");
-            break;
-        
-        case 'e':
-            printf("Esquerda\n");
-            break;
-        
-        case 'c':
-            printf("Cima\n");
-            break;
-        
-        case 'b':
-            printf("Baixo\n");
-            break;
-
-        default:
-            printf("Direcao invalida! Tente novamente.\nSaindo...\n\n");
-            break;
-        }
-}
-
-// Funcao auxiliar para se mover na diagonal
-void executar_mov_diag(char direcao_cb, char direcao_ed){
-    switch (direcao_cb)
-        {
-        case 'c':
-            if(direcao_ed == 'e'){
-                printf("Cima Esquerda\n");
-            }
-            else if(direcao_ed == 'd'){
-                printf("Cima Direita\n");
-            }
-            break;
-        
-        case 'b':
-            if(direcao_ed == 'e'){
-                printf("Baixo Esquerda\n");
-            }
-            else if(direcao_ed == 'd'){
-                printf("Baixo Direita\n");
-            }
-            break;
-
-        default:
-            printf("Direcao invalida! Tente novamente.\nSaindo...\n\n");
-            break;
-        }
-}
-
